359dbdc7209bde782d49a5f4b6e5012cd1f3be5d
Jason Anderson <jasonanderson@uchicago.edu>
2020-06-18 17:29:42 -0500

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-chameleon',
-    version='0.0.1',
+    version='0.0.2',
     description='Chameleon extensions for JupyterHub',
     url='https://github.com/chameleoncloud/jupyterhub-chameleon',
     author='Jason Anderson',
