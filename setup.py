7eaa322cd4d269c0312fe23e4542c73b805cd1e6
Jason Anderson <jasonanderson@uchicago.edu>
2021-04-01 10:06:49 -0500

    Remove dependency on keystoneauthenticator

--- setup.py
+++ setup.py
@@ -2,33 +2,35 @@ from glob import glob
 from setuptools import find_packages, setup
 
 setup(
-    name='jupyterhub-chameleon',
-    version='1.7.6',
-    description='Chameleon extensions for JupyterHub',
-    url='https://github.com/chameleoncloud/jupyterhub-chameleon',
-    author='Jason Anderson',
-    author_email='jasonanderson@uchicago.edu',
-    license='MIT',
+    name="jupyterhub-chameleon",
+    version="1.7.6",
+    description="Chameleon extensions for JupyterHub",
+    url="https://github.com/chameleoncloud/jupyterhub-chameleon",
+    author="Jason Anderson",
+    author_email="jasonanderson@uchicago.edu",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
-        'dockerspawner',
-        'jupyterhub',
-        'jupyterhub-keystoneauthenticator',
-        'keystoneauth1',
-        'oauthenticator',
-        'python-keystoneclient',
-        'tornado',
-        'traitlets',
+        "dockerspawner",
+        "jupyterhub",
+        "keystoneauth1",
+        "oauthenticator",
+        "python-keystoneclient",
+        "tornado",
+        "traitlets",
     ],
     entry_points={
-        'jupyterhub.authenticators': [
-            'chameleon = jupyterhub_chameleon.authenticator.keycloak:ChameleonKeycloakAuthenticator',
+        "jupyterhub.authenticators": [
+            "chameleon = jupyterhub_chameleon.authenticator.keycloak:ChameleonKeycloakAuthenticator",
         ],
-        'jupyterhub.spawners': [
-            'chameleon = jupyterhub_chameleon.spawner:ChameleonSpawner',
+        "jupyterhub.spawners": [
+            "chameleon = jupyterhub_chameleon.spawner:ChameleonSpawner",
         ],
     },
     data_files=[
-        ('share/jupyterhub_chameleon/templates', glob('share/jupyterhub_chameleon/templates/*.html')),
+        (
+            "share/jupyterhub_chameleon/templates",
+            glob("share/jupyterhub_chameleon/templates/*.html"),
+        ),
     ],
 )
