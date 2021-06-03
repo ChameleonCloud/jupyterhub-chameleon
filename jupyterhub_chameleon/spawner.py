8e98234e7dc6a00aeec617143f6a0a0c99d894c0
Jason Anderson <jasonanderson@uchicago.edu>
2021-06-03 17:41:31 -0500

    Mount user's main workspace dir at /share
    This only applies to named servers, aka Trovi/artifact servers.
    This allows a Trovi experiment environment to access stuff from the
    "default" environment, namely SSH keys are useful here.

--- jupyterhub_chameleon/spawner.py
+++ jupyterhub_chameleon/spawner.py
@@ -5,6 +5,7 @@ from traitlets import default, Bool, Dict, Unicode
 
 from .utils import Artifact
 
+
 class ChameleonSpawner(DockerSpawner):
     work_dir = Unicode(
         "/work",
@@ -13,14 +14,23 @@ class ChameleonSpawner(DockerSpawner):
         This directory will be mounted at ~/work inside the user's container
         and is intended to be a "long term" scratch directory unique to the
         user.
-        """
+        """,
+    )
+
+    share_dir = Unicode(
+        "/share",
+        config=True,
+        help="""Shared directory
+        This directory will contain the user's shared files across all of their
+        Jupyter servers.
+        """,
     )
 
     # Always remove stopped containers
     remove = Bool(True, config=True)
 
     # Default to JupyterLab
-    default_url = Unicode('/lab')
+    default_url = Unicode("/lab")
 
     # TODO: can enable picking different images here.
     #
@@ -41,12 +51,15 @@ class ChameleonSpawner(DockerSpawner):
     #         options['argv'] = shlex.split(arg_s)
     #     return options
 
-    @default('name_template')
+    _default_name_template = "{prefix}-{username}"
+    _named_name_template = "{prefix}-{username}-exp-{servername}"
+
+    @default("name_template")
     def _name_template(self):
         if self.name:
-            return '{prefix}-{username}-exp-{servername}'
+            return self._named_name_template
         else:
-            return '{prefix}-{username}'
+            return self._default_name_template
 
     extra_volumes = Dict(config=True, default_value={})
 
@@ -55,25 +68,41 @@ class ChameleonSpawner(DockerSpawner):
         vols = {}
         artifact = self.get_artifact()
         if not (artifact and artifact.ephemeral):
-            vols[self.name_template] = self._gen_volume_config(self.work_dir)
+            if self.name:
+                vols[self._named_name_template] = self._gen_volume_config(self.work_dir)
+                # Also mount the default directory as shared dir
+                vols[self._default_name_template] = self._gen_volume_config(
+                    self.share_dir
+                )
+            else:
+                vols[self._default_name_template] = self._gen_volume_config(
+                    self.work_dir
+                )
+
         vols.update(self.extra_volumes)
         return vols
 
-    @default('environment')
+    @default("environment")
     def _environment(self):
+        chown_extra = [self.work_dir]
+        if self.name:
+            chown_extra.append(self.share_dir)
         return {
-            'CHOWN_EXTRA': self.work_dir,
-            'CHOWN_EXTRA_OPTS': '-R',
+            "CHOWN_EXTRA": ",".join(chown_extra),
+            "CHOWN_EXTRA_OPTS": "-R",
             # Allow users to have sudo access within their container
-            'GRANT_SUDO': 'yes',
+            "GRANT_SUDO": "yes",
             # Enable JupyterLab application
-            'JUPYTER_ENABLE_LAB': 'yes',
+            "JUPYTER_ENABLE_LAB": "yes",
         }
 
-    resource_limits = Bool(config=True, default_value=True,
-        help='Whether to set default resource limits on the spawned servers.')
+    resource_limits = Bool(
+        config=True,
+        default_value=True,
+        help="Whether to set default resource limits on the spawned servers.",
+    )
 
-    @default('extra_host_config')
+    @default("extra_host_config")
     def _extra_host_config(self):
         """Configure docker host vars.
 
@@ -82,52 +111,56 @@ class ChameleonSpawner(DockerSpawner):
         many cores a container will be allowed to have in a CPU-bound scheduling
         situation, e.g. 100/100 = 1 core.
         """
-        host_config = {'network_mode': self.network_name}
+        host_config = {"network_mode": self.network_name}
 
         if self.resource_limits:
-            host_config.update({
-                'mem_limit': '1G',
-                'cpu_period': 100000, # nanosecs
-                'cpu_quota': 100000, # nanosecs
-            })
+            host_config.update(
+                {
+                    "mem_limit": "1G",
+                    "cpu_period": 100000,  # nanosecs
+                    "cpu_quota": 100000,  # nanosecs
+                }
+            )
 
         return host_config
 
-    @default('extra_create_kwargs')
+    @default("extra_create_kwargs")
     def _extra_create_kwargs(self):
         return {
             # Need to launch the container as root in order to grant sudo access
-            'user': 'root'
+            "user": "root"
         }
 
-    @default('image')
+    @default("image")
     def _image(self):
-        return os.environ['DOCKER_NOTEBOOK_IMAGE']
+        return os.environ["DOCKER_NOTEBOOK_IMAGE"]
 
-    @default('network_name')
+    @default("network_name")
     def _network_name(self):
-        return os.environ['DOCKER_NETWORK_NAME']
+        return os.environ["DOCKER_NETWORK_NAME"]
 
     def get_env(self):
         env = super().get_env()
 
         extra_env = {}
         # Rename notebook user (jovyan) to Chameleon username
-        extra_env['NB_USER'] = self.user.name
-        extra_env['OS_KEYPAIR_PRIVATE_KEY'] = f'{self.work_dir}/.ssh/id_rsa'
-        extra_env['OS_KEYPAIR_PUBLIC_KEY'] = f'{self.work_dir}/.ssh/id_rsa.pub'
+        extra_env["NB_USER"] = self.user.name
+        ssh_dir = self.share_dir if self.name else self.work_dir
+        extra_env["OS_KEYPAIR_PRIVATE_KEY"] = f"{ssh_dir}/.ssh/id_rsa"
+        extra_env["OS_KEYPAIR_PUBLIC_KEY"] = f"{ssh_dir}/.ssh/id_rsa.pub"
 
         # Add parameters for experiment import
         artifact = self.get_artifact()
         if artifact:
             deposition_url = artifact.deposition_url()
-            extra_env['ARTIFACT_DEPOSITION_URL'] = deposition_url
-            extra_env['ARTIFACT_DEPOSITION_REPO'] = artifact.deposition_repo
-            extra_env['ARTIFACT_ID'] = artifact.id
-            extra_env['ARTIFACT_OWNERSHIP'] = artifact.ownership
+            extra_env["ARTIFACT_DEPOSITION_URL"] = deposition_url
+            extra_env["ARTIFACT_DEPOSITION_REPO"] = artifact.deposition_repo
+            extra_env["ARTIFACT_ID"] = artifact.id
+            extra_env["ARTIFACT_OWNERSHIP"] = artifact.ownership
             self.log.info(
-                f'User {self.user.name} importing from '
-                f'{artifact.deposition_repo}: {deposition_url}')
+                f"User {self.user.name} importing from "
+                f"{artifact.deposition_repo}: {deposition_url}"
+            )
 
         env.update(extra_env)
 
@@ -142,11 +175,11 @@ class ChameleonSpawner(DockerSpawner):
     def _gen_volume_config(self, target):
         return dict(
             target=target,
-            driver=os.getenv('DOCKER_VOLUME_DRIVER', 'local'),
+            driver=os.getenv("DOCKER_VOLUME_DRIVER", "local"),
             driver_opts=self._docker_volume_opts(),
         )
 
     def _docker_volume_opts(self):
-        opt_str = os.getenv('DOCKER_VOLUME_DRIVER_OPTS', '')
-        tuples = [s.split('=') for s in opt_str.split(',') if s]
+        opt_str = os.getenv("DOCKER_VOLUME_DRIVER_OPTS", "")
+        tuples = [s.split("=") for s in opt_str.split(",") if s]
         return {t[0]: t[1] for t in tuples if len(t) == 2}
