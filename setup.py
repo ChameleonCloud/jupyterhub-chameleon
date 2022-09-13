e9878d42243304e0686730b0a26974041cad4916
Adam Cooper <adamcooper@uchicago.edu>
2022-09-13 14:41:56 -0500

    Pass name of file for artifact name storage to notebook server (#8)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="jupyterhub-chameleon",
-    version="2.0.8",
+    version="2.0.9",
     description="Chameleon extensions for JupyterHub",
     url="https://github.com/chameleoncloud/jupyterhub-chameleon",
     author="Jason Anderson",
