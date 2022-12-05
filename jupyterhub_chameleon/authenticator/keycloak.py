e36fbc39e2a70586c45cbd985aa571215bc06231
Adam Cooper <adamcooper@uchicago.edu>
2022-12-05 14:14:02 -0600

    Fix logout redirect URL

--- jupyterhub_chameleon/authenticator/keycloak.py
+++ jupyterhub_chameleon/authenticator/keycloak.py
@@ -216,9 +216,8 @@ class ChameleonKeycloakAuthenticator(OAuthenticator):
     def logout_redirect_url(self):
         params = {
             "client_id": self.client_id,
-            "redirect_uri": f"{self.keycloak_realm_url}/post-logout?client_id={self.client_id}",
         }
-        return f"{self.keycloak_realm_url}/protocol/openid-connect/logout?{urlencode(params)}"
+        return f"{self.keycloak_realm_url}/post-logout?{urlencode(params)}"
 
     async def authenticate(self, handler, data=None):
         """Authenticate with Keycloak."""
