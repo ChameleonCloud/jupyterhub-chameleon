7136dab870936bc6a35598a7d124d18ae85791db
Adam Cooper <adamcooper@uchicago.edu>
2022-12-05 14:14:57 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="jupyterhub-chameleon",
-    version="2.0.12",
+    version="2.0.13",
     description="Chameleon extensions for JupyterHub",
     url="https://github.com/chameleoncloud/jupyterhub-chameleon",
     author="Jason Anderson",
